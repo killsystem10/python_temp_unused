fb2ef2e5aec352e011f3d7ef5b83c89438053133
killsystem10 <killsystem10@outlook.com>
2023-01-13 10:59:26 +0900

    Modify E1.py

--- E1.py
+++ E1.py
@@ -3,6 +3,29 @@ nexh=0
 battery=0
 usebattery=0
 savebattery=0
+temp=0
 high=[]
 
-score=int(input())
+temp=int(input())
+high=input().split()
+high=list(map(int,high))
+high.append(0)
+
+
+
+for i in range(len(high)):
+    preh=high[i]
+    nexh=high[i+1]
+    if nexh==0:
+        break
+    else:
+        if preh<nexh:
+            temp=nexh-preh
+            usebattery=usebattery+2*temp
+        elif preh>nexh:
+            temp=preh-nexh
+            temp=temp//2
+            savebattery=savebattery+temp
+
+resultbattery=usebattery-savebattery
+print(resultbattery)
