d0b07fdc609f3a1b29e152ca22311bf757f9c92f
killsystem10 <killsystem10@outlook.com>
2023-01-13 10:15:57 +0900

    testing

--- temp.py
+++ temp.py
@@ -1,12 +1,7 @@
 preh=2
 nexh=3
-battery=0
 
-
-
-if preh>nexh:
-    temp=preh-nexh
-    battery=2*temp
-elif preh<nexh:
-    temp=nexh-preh
-    temp=temp/2
+temp=nexh-preh
+float(temp)
+temp=temp//2
+print(temp)
