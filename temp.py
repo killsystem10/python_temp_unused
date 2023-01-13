ec60019f921a791a056cf2fdb9e1f6273965b21e
killsystem10 <killsystem10@outlook.com>
2023-01-13 10:24:13 +0900

    Modify test.py

--- temp.py
+++ temp.py
@@ -1,12 +1,23 @@
-preh=2
-nexh=3
+preh=0
+nexh=0
 battery=0
+usebattery=0
+savebattery=0
+high=[10,6,10,6,8,15,10,13,12,20,0]
 
+for i in range(11):
+    preh=high[i]
+    nexh=high[i+1]
+    if nexh==0:
+        break
+    else:
+        if preh>nexh:
+            temp=preh-nexh
+            usebattery=usebattery+2*temp
+        elif preh<nexh:
+            temp=nexh-preh
+            temp=temp//2
+            savebattery=savebattery+temp//2
 
-
-if preh>nexh:
-    temp=preh-nexh
-    battery=2*temp
-elif preh<nexh:
-    temp=nexh-preh
-    temp=temp/2
+resultbattery=usebattery-savebattery
+print(resultbattery)
