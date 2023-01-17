75299995aa78fc1f89b3c44ac478df5f9b84bbd3
killsystem10 <killsystem10@outlook.com>
2023-01-17 10:02:00 +0900

    Revert "Delete C0.py, D0.py, E0.py, E1.py"
    This reverts commit ea0e6a13a2d8ca6c42a067780bf9cf972af0f227.

---
+++ E0.py
@@ -0,0 +1,23 @@
+preh=0
+nexh=0
+battery=0
+usebattery=0
+savebattery=0
+high=[10,6,10,6,8,15,10,13,12,20,0]
+
+for i in range(len(high)):
+    preh=high[i]
+    nexh=high[i+1]
+    if nexh==0:
+        break
+    else:
+        if preh<nexh:
+            temp=nexh-preh
+            usebattery=usebattery+2*temp
+        elif preh>nexh:
+            temp=preh-nexh
+            temp=temp//2
+            savebattery=savebattery+temp
+
+resultbattery=usebattery-savebattery
+print(resultbattery)
